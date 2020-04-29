80be74f704f07033c0d36fbeba27ff86145b6c84
yds12 <28710965+yds12@users.noreply.github.com>
2020-04-29 15:36:50 +0200

    Array, Union and Object types.

--- src/index.ts
+++ src/index.ts
@@ -25,13 +25,115 @@ let t: boolean;
 t = true;
 // t = 0; // error
 
-// Type checking also occurs in functions:
+// Type checking also occurs in functions. Here the argument par
+// must be of type number.
 function justReturn(par: number){
   return par;
 }
 
 justReturn(z); // okay
-// justReturn(t); // error
+// justReturn(t); // error (t is boolean)
+
+
+
+// ------------------------------------------------------------------------
+// Array, Union and Object types
+// ------------------------------------------------------------------------
+
+// Union types are types made out of combination of other types.
+let stringOrInt: string | number;
+stringOrInt = 10; // okay
+stringOrInt = 'a'; // also okay
+
+
+// Array types can be defined with the following syntax:
+let arr: [];
+let arrayOfAnything: any[];
+let arrayOfStrings: string[];
+
+// The type will be inferred based on the content, for example,
+// arrStr will be of type string[]:
+let arrStr = ['a', 'b'];
+
+function receivedStringArray(arr: string[]){};
+receivedStringArray(arrStr); // okay
+
+// Now if we wanted the array to be of string or number, we could declare
+// it explicitly
+let arrStrInt: (string | number)[] = ['a', 'b'];
+
+// but now this would fail, even though the actual array has only strings:
+// receivedStringArray(arrStrInt);
+
+// We can also count on tsc to infer the types of the elements of a typed
+// array:
+let newStrArr = ['a', 'b'];
+
+for(const str in newStrArr){
+  console.log(str.toUpperCase());
+}
+
+
+// You can declare object types.
+let obj: object;
+// obj = 3; // error
+// obj = 'a'; // error
+// obj = [1, 2]; // error
+obj = {}; // okay
+obj = { name: 'Lucas', age: 10 }; // okay
+
+class SomeClass{};
+obj = new SomeClass(); // okay
+
+// Now, the following code generates an error, because even though obj has
+// a property called name, we declared it generically just as object, which
+// does not necessarily has a property called name.
+obj = { name: 'Jose' };
+// let v = obj.name; // fails
+
+// This problem would not have happened if we just declare obj and let
+// tsc infer its type:
+// let obj = { name: 'Jose' }; // inferred type: { name: string }
+
+// We can make more specific object types.
+// The code below declares a variable person
+// which has a name property of type string, and an age property of type
+// number.
+let person: { name: string, age: number };
+person = { name: 'Jonas', age: 45 };
+console.log(person.name); // okay
+person.name = 'Maria'; // okay
+// person.name = 10; // error
+
+// In vanilla js, the following code would just print undefined, but tsc
+// throws a type error, because person's type has no id field:
+// console.log(person.id);
+
+// You can also declare and assign at the same time:
+let will: { name: string, age: number } =
+  { name: 'William', age: 77 };
+
+// This might seem useless, but it can actually be useful. For example,
+// when it comes to array types:
+let mark = { name: 'Marcus', favoriteValues: ['a', 'b'] };
+
+// mark.favoriteValues is inferred to be of type string[]. Maybe we
+// wanted it to be of type (string | number)[]. In that case, we have 
+// to do this:
+let mark2: { name: string, favoriteValues: (string | number)[] }
+  = { name: 'Marcus', favoriteValues: ['a', 'b'] };
+
+// Now:
+receivedStringArray(mark.favoriteValues); // okay: parameter is indeed string[]
+
+// error: parameter is (string | number)[]
+// receivedStringArray(mark2.favoriteValues);
+
+// But note that
+let someArray: any = [];
+receivedStringArray(someArray); // works: type any disables type-checking
+
+
 
 // ------------------------------------------------------------------------
 // Special types
@@ -63,7 +165,10 @@ var2 = mystery; // okay
 
 
 
+// ------------------------------------------------------------------------
 // Function types
+// ------------------------------------------------------------------------
+
 type Unary = (a: number) => number;
 let f: Unary;
 f = ()=>{ return 0; };
